858cf7146723acd9cdebf4d661fa365b0e6c6f92
kundan kumarjha <kundanjha1076@gmail.com>
2020-02-15 18:24:15 +0530

    challenges

---
+++ if.py
@@ -0,0 +1,5 @@
+age = int(input("Enter a your age "))
+if(age >18 and age < 31):
+    print("You can vote")
+else:
+    print("You can't vote")
---
+++ in_not_in.py
@@ -0,0 +1,11 @@
+# parrot = "Norwegian Blue"
+# letter = input("Enter a character ")
+# if letter in parrot:
+#     print("{} is in {}".format(letter,parrot))
+# else:
+#     print("I don't need the letter")
+activity = input("What would you like to do today? ")
+if "cinema" not in activity:
+    print("But I want to go to cinema")
+    a = "ACTIVITY"
+    print(a.casefold())
